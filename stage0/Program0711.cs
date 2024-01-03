f4355e9e774d47052b05fbac2ab47f09ff469138
EldadCohen98 <eldcohen@g.jct.ac.il>
2024-01-03 23:41:37 +0200

    Add project files.

---
+++ stage0/Program0711.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            welcome0711();
+            Welcome8917();
+        }
+
+        private static void welcome0711()
+        {
+            Console.WriteLine("Enter your name: ");
+            string des = Console.ReadLine();
+            Console.WriteLine(@"{0}, welcome to my first console appliction", des);
+        }
+
+        static partial void Welcome8917();
+    }
+}
