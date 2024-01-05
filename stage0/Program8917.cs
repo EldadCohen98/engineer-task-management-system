14296377eef7dbe23051f1a9f3092848b84e6482
chezky gordon <ygordon@g.jct.ac.il>
2024-01-05 11:42:17 +0200

    initial code + welcome function

---
+++ stage0/Program8917.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+
+        static partial void Welcome8917() 
+        
+        { Console.Write("my name is; "); }
+    }
+}
